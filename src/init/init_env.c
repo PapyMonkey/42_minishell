0cd88a3eb9f2b0b0246a0ca511f0cbc0996700e7
Anthony Guiri <exastir@icloud.com>
2022-12-04 17:58:19 +0100

    docs(parsing): add missing docs

--- src/init/init_env.c
+++ src/init/init_env.c
@@ -6,12 +6,18 @@
 /*   By: aguiri <aguiri@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 13:27:17 by aguiri            #+#    #+#             */
-/*   Updated: 2022/12/04 17:41:43 by aguiri           ###   ########.fr       */
+/*   Updated: 2022/12/04 17:52:23 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+@brief Creates a new t_env element, splitting the provided *char on the first
+		'=' character. Stores the output in a {key:value} pair.
+
+@param envp One element of the 2D array storing all the ENV variables.
+*/
 static struct s_env	*init_env_element(char *envp)
 {
 	t_env	*element;
