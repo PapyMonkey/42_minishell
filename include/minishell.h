03896edd1a52b226287468ef0e4a80422a8fef6a
Anthony Guiri <exastir@icloud.com>
2022-12-04 18:07:42 +0100

    feat: add free functions and doc

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mgerbaud <mgerbaud@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:36:19 by mgerbaud          #+#    #+#             */
-/*   Updated: 2022/12/04 17:44:41 by aguiri           ###   ########.fr       */
+/*   Updated: 2022/12/04 18:07:20 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,23 @@ void	err_malloc_exit(void);
 		exit the current process with exit(1).
 */
 void	err_put_exit(void);
+
+// ****************************************************************************
+// Functions - free.c
+
+/**
+@brief Frees up an env {key:value} pair in a t_list.
+
+@param env Environnement variable, stored as a {key:value} pair.
+*/
+void	free_env(void *env);
+
+/**
+@brief Frees up the t_var variable.
+
+@param var Variable that contains all the other useful ones.
+*/
+void	free_var(struct s_var *var);
 
 // ****************************************************************************
 // Functions
