57fb2b99e65927f07a2516d7f54f296a4494b927
Benjamin Gales <bgales@c1r6p2.42nice.fr>
2023-02-19 18:47:43 +0100

    debut gestions signaux (ctrl +d, ctrl +c & ctrl \)

--- src/main.c
+++ src/main.c
@@ -3,65 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgerbaud <mgerbaud@student.42nice.fr>      +#+  +:+       +#+        */
+/*   By: bgales <bgales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 16:36:09 by mgerbaud          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/12/05 11:19:33 by aguiri           ###   ########.fr       */
-=======
-/*   Updated: 2022/11/30 16:19:18 by aguiri           ###   ########.fr       */
->>>>>>> 2eca6d0 (feat(builtin): add several builtins functions and minor changes)
+/*   Updated: 2023/02/19 18:43:14 by bgales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(__attribute__((unused))int argc,
-		__attribute__((unused))char **argv,
-		char **envp)
+
+int	main(int argc, char **argv, char **env)
 {
-<<<<<<< HEAD
 	t_var	*var;
 	t_list	*tmp;
 	char	*str;
+	char	**test;
 
 	var = init(env);
+	signal(SIGINT, signal_handler);
+	signal(SIGQUIT, signal_handler);
 	// while (var->l_pth->next)
 	// {
 	// 	tmp = var->l_pth;
 	// 	printf("%s\n", (char *)var->l_pth->content);
 	// 	var->l_pth = tmp->next;
 	// }
-	/* while (1)
-	{
-		str = readline("$> ");
-		add_history((const char *)str);
-
-		if (!strcmp("pwd", str))
-			b_pwd(env);
-		
-		// test = ft_split(str, ' ');
-		// int i;
-		// i = -1;
-		// while (test[++i])
-		// 	printf("%s\n", test[i]);
-		// if (search_path(&hpaths, &cmd, str))
-		// 	exec_cmd(&cmd, av, env);
-	} */
-=======
-	t_paths		hpaths;
-	char		*str;
-	char		**test;
-
-	if (!get_path(envp, &hpaths))
-		return (0);
 	while (1)
 	{
 		str = readline("$> ");
 		add_history((const char *)str);
-		if (b_routine(str, envp))
-			continue ;
+		if (!str) // ctrl + d
+			exit (0);
+		if (str[0]) // sans cette condition print l'env si je fais entree.
+			b_routine(str, var);
+		test = ft_split(str, ' ');
+		int i;
+		i = -1;
+		// while (test[++i])
+		// 	printf("%s\n", test[i]);
+		// if (search_path(&hpaths, &cmd, str))
+		// 	exec_cmd(&cmd, av, env);
 	}
->>>>>>> 2eca6d0 (feat(builtin): add several builtins functions and minor changes)
 	return (0);
 }
