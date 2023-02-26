2e6ac589792036bc37d328ade71dd7ce4dcda1e1
Benjamin Gales <bgales@c1r6p2.42nice.fr>
2023-02-26 17:36:19 +0100

    ligne de commande decoupee facon shell et norm lexer OK

--- src/lexer.c
+++ src/lexer.c
@@ -6,131 +6,43 @@
 /*   By: bgales <bgales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/19 17:49:47 by bgales            #+#    #+#             */
-/*   Updated: 2023/02/23 16:45:45 by bgales           ###   ########.fr       */
+/*   Updated: 2023/02/26 15:36:27 by bgales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-enum{
-	WORD,
-	PIPE,
-	HERE_DOC,
-	REDIR_IN,
-	APPEND,
-	REDIR_OUT
-};
-
-struct s_lexer
-{
-	char *value;
-	int type;
-	struct s_lexer *next;
-}; typedef struct s_lexer t_lexer;
 
 
-int closed_quotes(char *str)
-{
-	int i;
 
-	i = -1;
-
-	while (str[++i])
-	{
-		if (str[i] == '\'')
-		{
-			i++;
-			while (str[i] && str[i] != '\'')
-				i++;
-			if (!str[i])
-				return (1);
-		}
-		if (str[i] == '\"')
-		{
-			i++;
-			while (str[i] && str[i] != '\"')
-				i++;
-			if (!str[i])
-				return (1);
-		}
-	}
-	return (0);
-}
-
-int	itter_quote(char *str, char c)
-{
-	int i;
-
-	i = 1;
-	i++;
-	while (str[i] && str[i] != c)
-		i++;
-	if(str[i] != '\0')
-		i++;
-	return (i);
-}
-
-int	enum_finder(char *str)
-{
-	if (*str == '|')
-		return (PIPE);
-	if (*str == '<' && *(str + 1) != '<')
-		return (REDIR_IN);
-	if (*str == '<' && *(str + 1) == '<')
-		return (HERE_DOC);
-	if (*str == '>' && *(str + 1) != '>')
-		return(REDIR_OUT);
-	if (*str == '>' && *(str + 1) == '>')
-		return(APPEND);
-	return (0);
-}
 
 int	word_count(char *str)
 {
 	int		i;
 	int		wc;
+	int		no_segf;
 
 	i = 0;
-	wc = 1;
-
-	while(str[i])
+	no_segf = 0;
+	wc = 0;
+	while (str[i])
 	{
-		while ((str[i] == '\'' || str[i] == '\"'))
+		while (str[i] && str[i] != 32)
 		{
-			if (i > 0 && str[i - 1] == 32)
-				wc++;
-			if (i > 0 && str[i - 1] != 32)
-				break;
-			i += itter_quote(str, str[i]);
-			if (str[i] != 32 && i > 0)
-			{
-				while (str[i] != 32 && str[i] != '\'' && str[i] != '\"' && str[i])
-					i++;
-			}
-		}
-		if (str[i] == 32)
-		{
-			while (str[i] == 32 && str[i])
+			if ((str[i] != '\'' && str[i] != '\"'))
 				i++;
-				wc++;
+			while (str[i] && (str[i] == '\'' || str[i] == '\"'))
+				i += itter_quote(&str[i], str[i], &no_segf);
 		}
-		if (enum_finder(str))
-		{
-			if (!(enum_finder(str) % 2))
-			{
-				wc++;
-				i++;
-			}
-			else
-				wc++;
-		}
-		if (str[i])
+		while (str[i] && str[i] == 32)
 			i++;
+		wc++;
 	}
+	printf("%d\n", wc);
 	return (wc);
 }
 
-char **to_split(char *str)
+char	**to_split(char *str)
 {
 	char	*start;
 	char	**ret;
@@ -138,56 +50,30 @@ char **to_split(char *str)
 	int		i;
 	int		tab;
 
-	len = 0;
-	i = 0;
-	tab = 0;
-	ret = malloc(sizeof(char *) * (word_count(str) + 1));
-	if (!ret)
-	{
-		printf("Malloc failure\n");
-		exit (0);
-	}
-
+	ret = malloc(sizeof(char *) * (word_count(str)
+				+ (len = 0) + (i = 0) + (tab = 0)));
 	while (str[i])
 	{
-		if (str[i] == '\'' || str[i] == '\"')
+		start = &str[i];
+		while (str[i] && str[i] != 32)
 		{
-			if(i > 0 && str[i - 1] != 32)
-				break;
-			start = &str[i];
-			i +=itter_quote(&str[i], str[i]);
-			len += itter_quote(&str[i], str[i]);
-			if (str[i] != 32)
-			{
-				while (str[i] && str[i] != 32)
-				{
-					len++;
-					i++;
-				}
-				ret[tab++] = ft_substr(start, 0, len);
-				len = 0;
-			}
-		}
-		if (str[i] && str[i] != 32)
-		{
-			start = &str[i];
-			while (str[i] && str[i] != 32)
-			{
-				i++;
+			if ((str[i] != '\'' && str[i] != '\"') && i++ != -1)
 				len++;
-			}
-			ret[tab++] = ft_substr(start, 0, len);
-			len = 0;
+			while (str[i] && (str[i] == '\'' || str[i] == '\"'))
+				i += itter_quote(&str[i], str[i], &len);
 		}
-		if (str[i])
+		ret[tab++] = ft_substr(start, 0, len);
+		len = 0;
+		while (str[i] && str[i] == 32)
 			i++;
 	}
+	ret[tab] = NULL;
 	return (ret);
 }
 
 char	*del_quotes(char *split)
 {
-	char *ret;
+	char	*ret;
 	int		i;
 	int		char_count;
 
@@ -202,7 +88,7 @@ char	*del_quotes(char *split)
 	ret = malloc(sizeof(char) * (char_count + 1));
 	char_count = 0;
 	i = 0;
-	while(split[i])
+	while (split[i])
 	{
 		if (split[i] != '\'' && split[i] != '\"')
 			ret[char_count++] = split[i];
@@ -212,30 +98,28 @@ char	*del_quotes(char *split)
 	return (ret);
 }
 
-t_lexer *ft_split_args(char *str)
+t_lexer	*ft_split_args(char *str)
 {
-	int	i;
-	t_lexer *ret;
+	int		i;
+	t_lexer	*ret;
 	char	**split;
 
 	i = -1;
-
 	if (!str)
 		return (NULL);
 	if (!!closed_quotes(str))
 		return (NULL);
 	str = ft_strtrim(str, " ");
 	split = to_split(str);
-	while(split[++i])
-		split[i] = del_quotes(split[i]); //leaks ici 
-	i = -1;
 	while (split[++i])
 		printf("%s\n", split[i]);
-
+	// while(split[++i])
+	// 	split[i] = del_quotes(split[i]); //leaks ici
+	i = -1;
 	return (ret);
 }
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	ft_split_args(argv[1]);
 }
