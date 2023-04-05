362a2a4a533b6920b83d8abd1967f2f62b3765e0
Anthony Guiri <exastir@icloud.com>
2023-04-05 22:34:28 +0200

    fix: pipe working fine

--- src/pipex/pipex.c
+++ src/pipex/pipex.c
@@ -6,13 +6,19 @@
 /*   By: bgales <bgales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 12:54:24 by aguiri            #+#    #+#             */
-/*   Updated: 2023/03/30 07:27:12 by aguiri           ###   ########.fr       */
+/*   Updated: 2023/04/05 22:32:49 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include "utils.h"
-#include <stdio.h>
+
+static void print_current_cmd(t_var *var)
+{
+	if (!var->next_command)
+		printf("current cmd : NULL\n");
+	else
+		printf("current cmd : %s\n", get_arg_content(var->next_command));
+}
 
 // Access and execute commands
 static void	exec_command(
@@ -42,9 +48,6 @@ static void	exec_command(
 	free(path);
 	if (!try_access)
 		err_put_exit_command_not_found(command[0]);
-	// NOTE: Peut etre deplace dans la fonction d'appel
-	// exec_redirect_fd(fd[READ_END], STDIN_FILENO);
-	// exec_redirect_fd(fd[WRITE_END], STDOUT_FILENO);
 	env = build_env_array(var->l_env);
 	execve(try_access, command, env);
 	free_2d_char(env);
@@ -52,25 +55,6 @@ static void	exec_command(
 	free(try_access);
 	exit(EXIT_SUCCESS);
 }
-
-// static void handle_redirections(
-// 	t_var *var,
-// 	int	index,
-// 	int fd_old,
-// 	int fd_new)
-// {
-// 	t_list	*previous_separator;
-//
-// 	previous_separator = get_previous_sep(var, index);
-// 	if (!previous_separator)
-// 	{
-// 		
-// 	}
-// 	else if (get_arg_type(previous_separator) == PIPE)
-// 	{
-//
-// 	}
-// }
 
 // static void	exec_routine(
 // 	t_var *var,
@@ -120,27 +104,20 @@ static void exec_child_routine(
 	int fd_parent,
 	int *fd_child)
 {
-	printf("Check child - current cmd : %s\n", get_arg_content(var->next_command));
-	exec_redirect_fd(fd_parent, READ_END); // Connecte l'entrée standard à fd_old (sortie précédente)
-    if (index < var->n_cmds) // Si ce n'est pas la dernière commande
-        exec_redirect_fd(fd_child[1], WRITE_END); // Connecte la sortie standard au pipe d'entrée
-	close(fd_child[0]);
-	close(fd_child[1]);
-
+	printf("Check child - ");
+	print_current_cmd(var);
+	exec_redirect_fd(fd_parent, STDIN_FILENO); // Connecte l'entrée standard à fd_parent (sortie précédente)
+    if (index < var->n_cmds - 1) // Si ce n'est pas la dernière commande
+	{
+		printf("Check child - pas la derniere commande\n");
+		exec_redirect_fd(fd_child[WRITE_END], STDOUT_FILENO); // Connecte la sortie standard au pipe d'entrée
+	}
+	close(fd_child[READ_END]);
+	close(fd_child[WRITE_END]);
 	exec_command(var, fd_child);
-	// perror("execv");
 	exit(EXIT_FAILURE);
 }
 
-// static void	exec_routine(
-// 	t_var *var,
-// 	int index)
-// {
-//
-// }
-
-// if (index >= var->n_cmds)
-// 	return ;
 // TODO: Function to initialize our fd
 // Check if :
 //		is_there_redirection ? 
@@ -156,6 +133,7 @@ void executor_v2(
 
 	if (pipe(fd_to_child) == -1)
 		err_put_exit();
+	printf("Check parents - index = %d\n", index);
 	pid = fork();
 	if (pid < 0)
 		err_put_exit();
@@ -167,20 +145,14 @@ void executor_v2(
 		close(fd_to_child[WRITE_END]);
 		if (waitpid(pid, NULL, 0) == -1)
 			exit(EXIT_FAILURE);
-		if (index < var->n_cmds)
+		if (index < var->n_cmds - 1)
 		{
-			printf("Check parents - index = %d\n", index);
 			var->next_command = get_command_next(var->l_arg);
+			printf("Check parents - ");
+			print_current_cmd(var);
 			executor_v2(var, ++index, fd_to_child[READ_END]);
 		}
 		else
-			close(fd_to_child[0]);
-		wait(NULL);
+			close(fd_to_child[READ_END]);
 	}
 }
-
-// Qu'est que j'ai a faire dans les process fils ?
-// - commande
-// - redirection in 
-// - redirection out
-// - heredoc
--- src/utils/get_command.c
+++ src/utils/get_command.c
@@ -6,7 +6,7 @@
 /*   By: aguiri <aguiri@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/25 23:51:05 by aguiri            #+#    #+#             */
-/*   Updated: 2023/03/30 07:30:53 by aguiri           ###   ########.fr       */
+/*   Updated: 2023/04/05 22:31:43 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list *get_command_next(t_list *l_arg)
 	t_list *tmp;
 
 	tmp = l_arg->next;
-	while (tmp && !is_command(tmp->content))
+	while (tmp && !is_command(tmp))
 		tmp = tmp->next;
 	return(tmp);
 }
