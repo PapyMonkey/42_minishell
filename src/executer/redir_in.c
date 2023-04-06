250bc9c085be765afed5b6c1d5d1c35a0c5884cf
Anthony Guiri <exastir@icloud.com>
2023-04-06 03:09:31 +0200

    refactor(executer): organizing functions by theme

--- src/executer/redir_in.c
+++ src/executer/redir_in.c
@@ -1,35 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   redir_heredoc.c                                    :+:      :+:    :+:   */
+/*   redir_in.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aguiri <aguiri@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/05 21:43:17 by aguiri            #+#    #+#             */
-/*   Updated: 2023/04/06 03:04:41 by aguiri           ###   ########.fr       */
+/*   Created: 2023/04/06 02:54:21 by aguiri            #+#    #+#             */
+/*   Updated: 2023/04/06 02:54:42 by aguiri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int redir_heredoc(
+static int redir_in(
 	t_var *var,
 	int fd_write)
 {
-	t_list *heredoc_delimiter;
+	t_list *file_to_open;
+	int		fd_read;
 
-	heredoc_delimiter = var->current_arg->next;
-	read_and_write_to_fd_heredoc_v2(STDIN_FILENO, fd_write, heredoc_delimiter);
-	var->current_arg = heredoc_delimiter->next;
+	file_to_open = var->current_arg->next;
+	fd_read = open(get_arg_content(file_to_open),
+			O_RDONLY);
+	if (fd_read < 0)
+		err_put_exit();
+	read_and_write_to_fd(fd_read, fd_write);
+	var->current_arg = file_to_open->next;
+	close(fd_read);
 	close(fd_write);
-	return (HERE_DOC);
+	return (REDIR_IN);
 }
 
-int redir_heredoc_handle(
+int redir_in_handle(
 	t_var *var,
 	int fd_write)
 {
-	if (get_arg_type(var->current_arg) == HERE_DOC)
-		return (redir_heredoc(var, fd_write));
+	if (get_arg_type(var->current_arg) == REDIR_IN)
+		return (redir_in(var, fd_write));
 	return (0);
 }
+
